b544d6c9fa08198984a2ba0ea7d35ac715cb7bcd
Vencounsel <155586120+Vencounsel@users.noreply.github.com>
2024-04-03 18:51:23 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {
-			colors: {
-				bg: 'var(--background-color)',
-        			textDark: 'var(--text-dark)',
-        			textLight: '#FFFFFF',
-			        primaryOrange: 'var(--orange)',
-			        borderLight: '#EEEEE2',
-			        borderDark: 'var(--border-dark)',
-			        borderDarkHover: 'var(--border-dark-hover)',
-			        blue: 'var(--blue)',
-			        lightBlue: 'var(--light-blue)',
-			        pink: 'var(--pink)',
-			        pinkHover: 'var(--pink-hover)',
-			        dangerLight: '#FF6B6B'
-			}
-		}
-	},
-	plugins: []
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        bg: 'var(--background-color)',
+        textDark: 'var(--text-dark)',
+        textLight: '#FFFFFF',
+        primaryOrange: 'var(--orange)',
+        borderLight: '#EEEEE2',
+        borderDark: 'var(--border-dark)',
+        borderDarkHover: 'var(--border-dark-hover)',
+        blue: 'var(--blue)',
+        lightBlue: 'var(--light-blue)',
+        pink: 'var(--pink)',
+        dangerLight: '#FF6B6B'
+      }
+    }
+  },
+  plugins: []
 };
